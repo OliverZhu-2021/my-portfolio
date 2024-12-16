14357651dfe034c0b58d1fb0f16a5ea29971ba0d
Hao Zhu <zhuhao_0227@sina.com>
2024-12-16 14:32:08 +0800

    Add conditional render function in nav file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './nav/nav';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    border: 0;
+    box-sizing: border-box;
+    min-height: 100vh;
+    width: 100vw;
+  }
+`
 
 const App = () => {
   
   return (
     <>
-      <Nav></Nav>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Nav />
+        
+      </BrowserRouter>
+      
     </>
   )
 }
