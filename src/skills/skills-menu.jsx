b60d683403787835f1626112151977f28d12b0ad
Hao Zhu <zhuhao_0227@sina.com>
2024-12-25 16:44:52 +0800

    Add skills-menu

--- src/skills/skills-menu.jsx
+++ src/skills/skills-menu.jsx
@@ -1,11 +1,63 @@
-import '../style/skills-menu.css'
+import '../style/skills-menu.css';
+import classNames from 'classnames';
+import skills from './skills-data';
+import frontendIcon from '../assets/eagle-emblem.png';
+import backendIcon from '../assets/hawk-emblem.png';
+import React, { Component, useState } from 'react';
 
 const SkillsMenu = () => {
-  return(
-    <>
-    
-    </>
-  )
-};
+  const [activeMenuItem, setActiveMenuItem] = useState(1)
+
+  const handleMenuItemClick = (menuItem) => {
+    setActiveMenuItem(menuItem);
+  };
+
+  const menuItems = ['FRONT-END', 'BACK-END'];
+  const currentIcon = activeMenuItem === 1 ? frontendIcon : backendIcon;
+  
+  return (
+    <div className='skill-menu'>
+      {
+        menuItems.map((item, index) => (
+          <div
+            key={index}
+            className={classNames('skill-item', {
+              activeSkill: activeMenuItem === index + 1,
+            })}
+            onClick={() => handleMenuItemClick(index + 1)}
+          >
+            <h2 className='skill-title'>{item}</h2>
+          </div>
+        ))
+      }
+      <img className='skill-icon' src={currentIcon} alt='current skill'/>
+      <div
+        className='skill-sub-container'
+      >
+        {
+          skills[activeMenuItem].map((skill, index) => (
+            <div
+              key={index}
+              className={`skills-sub-container-${activeMenuItem}`}
+            >
+              <h3>{skill.title}</h3>
+              <div className='level-container'>
+                {[...Array(6)].map((_, i) => (
+                  <div
+                    key={i}
+                    className={`level-point ${
+                      i < skill.level ? 'filled' : 'unfilled'
+                    }`}
+                  >
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
 
 export default SkillsMenu
